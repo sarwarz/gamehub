b80894f31af7010e67ccd710e883ae2dbb0d9fb1
sarwarz <sarwarzahan.cse@gmail.com>
2026-01-16 23:06:16 +0600

    update home route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\ProductDeveloperController;
 use App\Http\Controllers\ProductPublisherController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 
